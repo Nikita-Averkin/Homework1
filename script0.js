2e9d57729704198858691b9a00c0e9d383dffb31
Nikita-Averkin <averkin.n@list.ru>
2024-09-19 18:22:09 +0300

    Change the file script0.js

--- script0.js
+++ script0.js
@@ -1,1 +1,34 @@
+let i = 0;
+while (i < 2) {
+    console.log("Привет");
+    i++;
+}
+
+let a = 1;
+while (a <= 5) {
+    console.log(a);
+    a++;
+}
+
+let b = 7;
+while (b <= 25) {
+    console.log(b);
+    b++;
+}
+
+const obj = {
+    Коля: 200,
+    Вася: 300,
+    Петя: 400.
+}
+for (let key in obj) {
+    console.log (key, obj[key]);
+}
+
+let n = 1000; f = 0;
+do { n = n / 2; f++ }
+while (n > 50) {
+ console.log ("num:" + n + "Iterators:" +f);
+}
+
 
